ca88e70f8c1b343636fb67af619015015b0969d4
craftablescience <me@craftablescience.info>
2024-06-29 03:36:33 -0400

    fix: don't use missing macro in apple hack code

--- patches/cmp_core/source/core_simd_apple.cpp
+++ patches/cmp_core/source/core_simd_apple.cpp
@@ -4,30 +4,30 @@
 
 #include "core_simd.h"
 
-CGU_FLOAT sse_bc1ComputeBestEndpoints(CGU_FLOAT endpointsOut[2],
-                                      CGU_FLOAT endpointsIn[2],
-                                      CGU_FLOAT prj[16],
-                                      CGU_FLOAT prjError[16],
-                                      CGU_FLOAT preMRep[16],
-                                      int       numColours,
-                                      int       numPoints)
+float sse_bc1ComputeBestEndpoints(float endpointsOut[2],
+                                  float endpointsIn[2],
+                                  float prj[16],
+                                  float prjError[16],
+                                  float preMRep[16],
+                                  int       numColours,
+                                  int       numPoints)
 {
-	CGU_FLOAT minError = MAX_ERROR;
+	float minError = MAX_ERROR;
 
-	static const CGU_FLOAT searchStep = 0.025f;
+	static const float searchStep = 0.025f;
 
-	const CGU_FLOAT lowStart  = (endpointsIn[0] - 2.0f * searchStep > 0.0f) ? endpointsIn[0] - 2.0f * searchStep : 0.0f;
-	const CGU_FLOAT highStart = (endpointsIn[1] + 2.0f * searchStep < 1.0f) ? endpointsIn[1] + 2.0f * searchStep : 1.0f;
+	const float lowStart  = (endpointsIn[0] - 2.0f * searchStep > 0.0f) ? endpointsIn[0] - 2.0f * searchStep : 0.0f;
+	const float highStart = (endpointsIn[1] + 2.0f * searchStep < 1.0f) ? endpointsIn[1] + 2.0f * searchStep : 1.0f;
 
-	CGU_FLOAT lowStep  = lowStart;
-	CGU_FLOAT highStep = highStart;
+	float lowStep  = lowStart;
+	float highStep = highStart;
 
 	for (int low = 0; low < 8; ++low)
 	{
 		for (int high = 0; high < 8; ++high)
 		{
 			// compute an error for the current pair of end points.
-			CGU_FLOAT error = cpu_RampSrchW(prj, prjError, preMRep, minError, lowStep, highStep, numColours, numPoints);
+			float error = cpu_RampSrchW(prj, prjError, preMRep, minError, lowStep, highStep, numColours, numPoints);
 
 			if (error < minError)
 			{
@@ -46,30 +46,30 @@ CGU_FLOAT sse_bc1ComputeBestEndpoints(CGU_FLOAT endpointsOut[2],
 	return minError;
 }
 
-CGU_FLOAT avx_bc1ComputeBestEndpoints(CGU_FLOAT endpointsOut[2],
-                                      CGU_FLOAT endpointsIn[2],
-                                      CGU_FLOAT prj[16],
-                                      CGU_FLOAT prjError[16],
-                                      CGU_FLOAT preMRep[16],
-                                      int       numColours,
-                                      int       numPoints)
+float avx_bc1ComputeBestEndpoints(float endpointsOut[2],
+                                  float endpointsIn[2],
+                                  float prj[16],
+                                  float prjError[16],
+                                  float preMRep[16],
+                                  int       numColours,
+                                  int       numPoints)
 {
-	CGU_FLOAT minError = MAX_ERROR;
+	float minError = MAX_ERROR;
 
-	static const CGU_FLOAT searchStep = 0.025f;
+	static const float searchStep = 0.025f;
 
-	const CGU_FLOAT lowStart  = (endpointsIn[0] - 2.0f * searchStep > 0.0f) ? endpointsIn[0] - 2.0f * searchStep : 0.0f;
-	const CGU_FLOAT highStart = (endpointsIn[1] + 2.0f * searchStep < 1.0f) ? endpointsIn[1] + 2.0f * searchStep : 1.0f;
+	const float lowStart  = (endpointsIn[0] - 2.0f * searchStep > 0.0f) ? endpointsIn[0] - 2.0f * searchStep : 0.0f;
+	const float highStart = (endpointsIn[1] + 2.0f * searchStep < 1.0f) ? endpointsIn[1] + 2.0f * searchStep : 1.0f;
 
-	CGU_FLOAT lowStep  = lowStart;
-	CGU_FLOAT highStep = highStart;
+	float lowStep  = lowStart;
+	float highStep = highStart;
 
 	for (int low = 0; low < 8; ++low)
 	{
 		for (int high = 0; high < 8; ++high)
 		{
 			// compute an error for the current pair of end points.
-			CGU_FLOAT error = cpu_RampSrchW(prj, prjError, preMRep, minError, lowStep, highStep, numColours, numPoints);
+			float error = cpu_RampSrchW(prj, prjError, preMRep, minError, lowStep, highStep, numColours, numPoints);
 
 			if (error < minError)
 			{
@@ -88,30 +88,30 @@ CGU_FLOAT avx_bc1ComputeBestEndpoints(CGU_FLOAT endpointsOut[2],
 	return minError;
 }
 
-CGU_FLOAT avx512_bc1ComputeBestEndpoints(CGU_FLOAT endpointsOut[2],
-                                      CGU_FLOAT endpointsIn[2],
-                                      CGU_FLOAT prj[16],
-                                      CGU_FLOAT prjError[16],
-                                      CGU_FLOAT preMRep[16],
-                                      int       numColours,
-                                      int       numPoints)
+float avx512_bc1ComputeBestEndpoints(float endpointsOut[2],
+                                     float endpointsIn[2],
+                                     float prj[16],
+                                     float prjError[16],
+                                     float preMRep[16],
+                                     int       numColours,
+                                     int       numPoints)
 {
-	CGU_FLOAT minError = MAX_ERROR;
+	float minError = MAX_ERROR;
 
-	static const CGU_FLOAT searchStep = 0.025f;
+	static const float searchStep = 0.025f;
 
-	const CGU_FLOAT lowStart  = (endpointsIn[0] - 2.0f * searchStep > 0.0f) ? endpointsIn[0] - 2.0f * searchStep : 0.0f;
-	const CGU_FLOAT highStart = (endpointsIn[1] + 2.0f * searchStep < 1.0f) ? endpointsIn[1] + 2.0f * searchStep : 1.0f;
+	const float lowStart  = (endpointsIn[0] - 2.0f * searchStep > 0.0f) ? endpointsIn[0] - 2.0f * searchStep : 0.0f;
+	const float highStart = (endpointsIn[1] + 2.0f * searchStep < 1.0f) ? endpointsIn[1] + 2.0f * searchStep : 1.0f;
 
-	CGU_FLOAT lowStep  = lowStart;
-	CGU_FLOAT highStep = highStart;
+	float lowStep  = lowStart;
+	float highStep = highStart;
 
 	for (int low = 0; low < 8; ++low)
 	{
 		for (int high = 0; high < 8; ++high)
 		{
 			// compute an error for the current pair of end points.
-			CGU_FLOAT error = cpu_RampSrchW(prj, prjError, preMRep, minError, lowStep, highStep, numColours, numPoints);
+			float error = cpu_RampSrchW(prj, prjError, preMRep, minError, lowStep, highStep, numColours, numPoints);
 
 			if (error < minError)
 			{
